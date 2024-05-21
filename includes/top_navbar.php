19fc8f7b41dbcc3dd9553d9b2159248df176a196
majpotatooo <majella.jereza.phowzzz@gmail.com>
2024-05-21 14:05:13 +0900

    edited top navbar

--- includes/top_navbar.php
+++ includes/top_navbar.php
@@ -8,13 +8,22 @@
             text-align: right;
             color: #ffffff;
             background-color: #143A1F;
-            padding: 5px;
-            font-size: 13px;
+            padding: 2px;
+            font-size: 12px;
+            height: 26px;
             font-family: Arial, Helvetica, sans-serif;
             /* Added responsive styles */
             max-width: 100%; /* Ensures the container doesn't overflow */
             box-sizing: border-box; /* Keeps padding inside the width */
             overflow: hidden; /* Prevents overflow of content */
+        }
+
+        #headerStyle {
+            background-color: #D8F1B9;
+            max-width: 100%;
+            color: #D8F1B9;
+            height: 10px;
+            font-size: 7px;
         }
     </style>
 </head>
@@ -30,6 +39,9 @@
             // Display the result
             echo "<p>PHILIPPINE STANDARD TIME: $completeDateTime</p>";
         ?>
+    </div>
+    <div id="headerStyle">
+        <p>LOWER STYLE</p>
     </div>
 </body>
 </html>
